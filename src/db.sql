5e02f1fff632be927bda1de97c5cc7fd7a40940d
Gustavo Herrera <tavoxr23@gmail.com>
2020-03-04 10:42:23 -0600

    put route

--- src/db.sql
+++ src/db.sql
@@ -15,3 +15,31 @@ DESCRIBE employees;
 
 
 INSERT INTO employees(name,salary) VALUES('Gustavo Herrera',15000),("Maria Lopez",12000),("Juan Pablo", 30000);
+
+
+
+CREATE PROCEDURE employeeAddOrEdit(
+    IN _id INT,
+    IN _name VARCHAR(45),
+    IN _salary INT
+
+
+)
+
+BEGIN
+    IF _id = 0 THEN
+        INSERT INTO employees(name,salary) 
+        VALUES( _name, _salary);
+        SET _id = LAST_INSERT_ID();
+    ELSE 
+        UPDATE employees
+        SET
+            name = _name,
+            salary = _salary
+            WHERE id = _id;
+    END IF;
+
+    SELECT _id AS id;
+END
+
+DELIMITER
--- src/routes/employees.js
+++ src/routes/employees.js
@@ -14,6 +14,53 @@ router.get('/',(req,res)=>{
    })
 })
 
+router.get('/:id',(req,res)=>{
+
+    const {id} = req.params
+    
+    mysqlConnection.query('SELECT * FROM employees WHERE id = ?',[id],(err,rows,field)=>{
+
+        if(!err){
+            res.json(rows[0])
+        }else{
+            console.log(err)
+        }
+    })
+
+})
+
+router.post('/',(req,res)=>{
+    const {id,name,salary} = req.body
+const query= "CALL employeeAddOrEdit(?, ? , ?);"
+
+    mysqlConnection.query(query,[id,name,salary], (err,rows,fields)=>{
+
+        if(!err){
+            res.json({Status: 'Employee Saved'})
+        }else{
+            console.log(err)
+        }
+    })
+
+})
+
+router.put('/:id',(req,res)=>{
+
+    const{name, salary} = req.body
+    const {id} = req.params
+
+    const query= "CALL employeeAddOrEdit(?, ? , ?);"
+    mysqlConnection.query(query,[id,name,salary], (err,rows,fields)=>{
+
+        if(!err){
+            res.json({Status: 'Employee Updated'})
+        }else{
+            console.log(err)
+        }
+    })
+
+})
+
 
 
 module.exports =  router
